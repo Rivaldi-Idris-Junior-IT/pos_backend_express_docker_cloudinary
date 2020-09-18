0fb528675897a21d6acfb1da17eb0476960ca3f1
Rivaldi-Idris-Junior-IT <rivaldi905@gmail.com>
2020-09-18 21:40:46 +0700

    tambah validate

--- src/Routes/Product.js
+++ src/Routes/Product.js
@@ -9,8 +9,8 @@ const Routes = express.Router()
 // Import untuk produk
 Routes.get("/", controller.all)
 Routes.get("/search/:nama", controller.search)
-Routes.post("/", upload.single("link_gambar"), controller.add)
-Routes.put("/", upload.single("link_gambar"), controller.Update)
+Routes.post("/", validate, upload.single("link_gambar"),chache, controller.add)
+Routes.put("/", validate, upload.single("link_gambar"),chache, controller.Update)
 Routes.delete("/delete/:id", chache, controller.delete)
 Routes.get("/useJoin",controller.joinall)
 
